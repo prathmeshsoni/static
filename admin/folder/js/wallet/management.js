136a59721f1308240572decb123d187791711a2b
prathmesh <prathmesh.soni51@gmail.com>
2023-08-13 19:24:38 +0530

    Update at 13-08-2023

--- admin/folder/js/wallet/management.js
+++ admin/folder/js/wallet/management.js
@@ -18,6 +18,8 @@ function gettransaction(){
           dataType: 'JSON',
           success: function (data) {
                 remove_all()
+                var table = $('#myTable_2').DataTable();
+                table.clear();
                 $('#date-iss').val(data.hid);
                 if (data.status === true) {
 
@@ -29,9 +31,11 @@ function gettransaction(){
                     if (data.not_transfer.length > 0) {
                         for (var i = 0; i < data.not_transfer.length; i++) {
                             let date_name = data.not_transfer[i]['date']
+
+                            transfer_table(data.not_transfer[i]['price'], date_name, 0, 2)
                             transfer_table('', date_name, 0, 0)
 
-                            for (var j = 0; j < data.not_transfer[i]['data_list'].length; j++) {
+                            for (var j = data.not_transfer[i]['data_list'].length-1; j>=0 ; j--) {
                                 transfer_table(data.not_transfer[i]['data_list'][j], date_name, 0, 1)
                             }
                         }
@@ -46,11 +50,12 @@ function gettransaction(){
                             let date_name = data.transfer[i]['date']
                             transfer_table('', date_name, 1, 0)
 
-                            for (var j = 0; j < data.transfer[i]['data_list'].length; j++) {
+                            for (var j = data.transfer[i]['data_list'].length-1; j>=0 ; j--) {
                                 transfer_table(data.transfer[i]['data_list'][j], date_name, 1,  1)
                             }
                         }
                     }
+
                     else {
                         $('#nodatass_1').css('text-align', 'center')
                         $('#nodatass_1').css('display', 'block')
@@ -64,6 +69,10 @@ function gettransaction(){
                     $('#nodatass_1').css('text-align', 'center')
                     $('#nodatass_1').css('display', 'block')
                 }
+                table.draw();
+                // var dd = document.querySelectorAll('#myTable_2 tbody tr td')
+                // for(var k= 1; k < dd.length; k+=3){dd[k].style.color = '#ff0000';}
+                // for(var k= 2; k < dd.length; k+=3){dd[k].style.color = '#000fff';}
                 $("#balance-1").val(data.price['temp_add']);
                 $("#balance-2").val(data.price['temp_sub']);
                 $("#balance-3").val(data.price['total_amount']);
@@ -94,11 +103,38 @@ function transfer_table(dataArray, date_id, id, check){
         var rows = tableBody.querySelector('[id="date-' + date_id + '"]')
         tableBody.insertBefore(newRow, rows.nextSibling);
     }
-    else {
+    else if(check === 0){
         const newRow_ = document.createElement("tr");
         newRow_.id = 'date-'+ date_id;
         newRow_.innerHTML = '<td>' + date_id + '</td>';
         tableBody.insertBefore(newRow_, tableBody.firstChild);
+    }
+    else{
+        var table = $('#myTable_2').DataTable();
+        var newData = [
+            '<td>' + date_id + '</td>'
+        ]
+        var newData1 = [
+            '<td><span class="income-cell">' + dataArray.temp_add + ' <i class="fa fa-rupee"></i></span></td>'
+        ]
+        var newData2 = [
+            '<td><span class="expense-cell">' + dataArray.temp_sub + ' <i class="fa fa-rupee"></i></span></td>'
+        ]
+        // var newData = [
+        //   '<td>New Date 1</td>',
+        //   '<td>New Price 1 <i class="fa fa-rupee"></i></td>',
+        //   '<td>New Price 2 <i class="fa fa-rupee"></i></td>'
+        // ];
+        // table.row.add(newData.join(''));
+        table.row.add([newData, newData1, newData2]).draw();
+
+        // tableBody = document.querySelector("#myTable_2 tbody");
+        // const newRow_ = document.createElement("tr");
+        // newRow_.innerHTML = '<td>' + date_id + '</td>' +
+        //     '<td style="color: #000fff !important;" >' + dataArray.temp_add + '<i class="fa fa-rupee"></i></td>' +
+        //     '<td style="color: #ff0000 !important;" >' + dataArray.temp_sub + '<i class="fa fa-rupee"></i></td>';
+        //
+        // tableBody.insertBefore(newRow_, tableBody.firstChild);
     }
 }
 
@@ -111,10 +147,12 @@ function formatDate11(dateString) {
 function createTable(i, formattedDate, date_id, id) {
     let color_ = '#000'
     let che = '#a3a6a4';
+    var f_date = formatDate(i.date_name.replace('Z', ''))
+    var f_date_1 = `${f_date[1]} ${f_date[0]}`;
     if (id === 1){
         return [(
             '<td><i style="margin-right: 20px;" class="fa fa-info-circle" aria-hidden="true"></i></td>' +
-            '<td>' + formatDate11(i.date_name.replace('Z', '')) + '</td>' +
+            '<td>' + f_date[0] + '</td>' +
             '<td>' + i.from_account.account_name + '</td>' +
             '<td>' + i.to_account.account_name + '</td>' +
             '<td style="color: '+ color_ +' !important;">' + i.amount + ' ₹</td>' +
@@ -125,7 +163,7 @@ function createTable(i, formattedDate, date_id, id) {
             '            Edit\n' +
             '    </span>' +
             '    </a>' +
-            '    <a href="javascript:void(0)" onclick="Delete(' + i.id + ',' + 1 + ')" class="bg-info ml-2">' +
+            '    <a href="javascript:void(0)" onclick="Delete(' + i.id + ',' + 1 + ', \'' + f_date_1 + '\')" class="bg-info ml-2">' +
             '        <span class="label label-danger">' +
             '            Delete' +
             '        </span>' +
@@ -147,7 +185,7 @@ function createTable(i, formattedDate, date_id, id) {
 
         return [(
             '<td><i style="margin-right: 20px;" class="fa fa-info-circle" aria-hidden="true"></i></td>' +
-            '<td>' + formatDate11(i.date_name.replace('Z', '')) + '</td>' +
+            '<td>' + f_date[0] + '</td>' +
             '<td>' + i.category.cat_name + '</td>' +
             '<td>' + i.account.account_name + '</td>' +
             '<td style="color: '+ color_ +' !important;">' + i.amount + ' ₹</td>' +
@@ -158,7 +196,7 @@ function createTable(i, formattedDate, date_id, id) {
             '            Edit\n' +
             '    </span>' +
             '    </a>' +
-            '    <a href="javascript:void(0)" onclick="Delete(' + i.id + ',' + 1 + ')" class="bg-info ml-2">' +
+            '    <a href="javascript:void(0)" onclick="Delete(' + i.id + ',' + 1 + ', \'' + f_date_1 + '\')" class="bg-info ml-2">' +
             '        <span class="label label-danger">' +
             '            Delete' +
             '        </span>' +
@@ -296,11 +334,22 @@ function updateModel(id, chek_1) {
 
 
 function Delete(id, h_id, s_id){
-    // updateModel(id, 0)
-    var serializedData = form_se("#Addcategory");
+    var massage_1 = ''
+    var massage_2 = ''
+    var massage_3 = ''
+    if (s_id === 0){
+        massage_1 = "You will not be able to recover Transaction No. (" + h_id + ")";
+        massage_2 = "Your Transaction No. (" + h_id + ") has been deleted.";
+        massage_3 = 'Transaction No. (' + h_id + ') Delete Successfully ✔';
+    }
+    else{
+        massage_1 = "You will not be able to recover Transaction Date of\n (" + s_id + ")";
+        massage_2 = "Your Transaction (" + s_id + ") has been deleted.";
+        massage_3 = 'Transaction (' + s_id + ') Delete Successfully ✔';
+    }
     swal({
         title: "Are you sure?",
-        text: "You will not be able to recover Transaction No. (" + h_id + ")",
+        text: massage_1,
         type: "warning",
         showCancelButton: true,
         confirmButtonColor: "#DD6B55",
@@ -310,7 +359,7 @@ function Delete(id, h_id, s_id){
         closeOnCancel: false
     }, function(isConfirm){
         if (isConfirm) {
-            swal("Deleted!", "Your Transaction No. (" + h_id + ") has been deleted.", "success");
+            swal("Deleted!", massage_2, "success");
             var csrf_token = $('input[name="csrfmiddlewaretoken"]').val();
             var date_val = $('#date-iss').val();
             $.ajax({
@@ -330,7 +379,7 @@ function Delete(id, h_id, s_id){
                             $('#tra_' + id + '').remove();
                             $.toast({
                                 heading: 'Success ',
-                                text: 'Transaction No. (' + h_id + ') Delete Successfully ✔',
+                                text: massage_3,
                                 position: 'top-right',
                                 loaderBg: '#fc4b6c !important',
                                 icon: 'success',
@@ -609,6 +658,8 @@ function renderTableRows(dataArray, id) {
 function createTableRow(i, formattedDate, id) {
     var searchValue = document.querySelector('input[name="search-param"]');
     var value_name = $('[name="id"]').val();
+    formattedDate = formatDate(i.date_name.replace('Z', ''))
+    var formattedDate_1 = `${formattedDate[1]} ${formattedDate[0]}`;
     if (searchValue || value_name ){
         if(searchValue){
             searchValue = searchValue.value;
@@ -669,7 +720,7 @@ function createTableRow(i, formattedDate, id) {
             '            Edit\n' +
             '    </span>' +
             '    </a>' +
-            '    <a href="javascript:void(0)" onclick="Delete(' + id + ',' + 1 + ')" class="bg-info ml-2">' +
+            '    <a href="javascript:void(0)" onclick="Delete(' + id + ',' + 1 + ', \'' + formattedDate_1 + '\')" class="bg-info ml-2">' +
             '        <span class="label label-danger">' +
             '            Delete' +
             '        </span>' +
@@ -707,7 +758,7 @@ function createTableRow(i, formattedDate, id) {
             '            Edit\n' +
             '    </span>' +
             '    </a>' +
-            '    <a href="javascript:void(0)" onclick="Delete(' + id + ',' + 1 + ')" class="bg-info ml-2">' +
+            '    <a href="javascript:void(0)" onclick="Delete(' + id + ',' + 1 + ', \'' + formattedDate_1 + '\')" class="bg-info ml-2">' +
             '        <span class="label label-danger">' +
             '            Delete' +
             '        </span>' +
@@ -721,7 +772,7 @@ function createTableRow(i, formattedDate, id) {
 
 function formatDate(inputDate) {
     const date = new Date(inputDate);
-    const day = date.getDate()
+    const day = date.getDate().toString().padStart(2, '0')
     const month = date.toLocaleString('default', { month: 'long' });
     const year = date.getFullYear();
     const hours = date.getHours();
